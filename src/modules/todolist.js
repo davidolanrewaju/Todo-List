6c3683eab4756ffb7612e66440855d2591dd79a1
Dave <davidolanrewaju538@gmail.com>
2023-04-07 11:10:11 +0100

    Update src files

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -3,6 +3,8 @@ const refreshBtn = document.querySelector('.refresh');
 const addToList = document.querySelector('#add-to-list');
 const addBtn = document.querySelector('.add-btn');
 
+let todoList = [];
+
 function saveData() {
   localStorage.setItem('todolist', JSON.stringify(todoList));
 }
@@ -10,11 +12,9 @@ function saveData() {
 function loadData() {
   const savedData = JSON.parse(localStorage.getItem('todolist'));
   if (savedData) {
-      todoList = savedData;
-    }
+    todoList = savedData;
+  }
 }
-
-let todoList = [];
 
 // Add a clearList function to remove all child nodes from the displayTasks element
 function clearList() {
@@ -31,42 +31,89 @@ function renderList() {
     const displayTask = document.createElement('div');
     displayTask.classList.add('display-task');
     displayTask.innerHTML += `
-                <input class = 'checkbox' data-index = '${i}' type="checkbox" ${todoList[i].completed ? 'checked' : ''}>
-                <p>${todoList[i].description}</p>
-                <span class="material-icons">&#xe5d4;</span>
+                <div class="check-task">
+                  <input class = 'checkbox' data-index = '${i}' type="checkbox" ${todoList[i].completed ? 'checked' : ''}>
+                  <p data-index = '${i}'>${todoList[i].description}</p>
+                </div>
+                <div class="task-icons">
+                  <span class="material-icons edit" data-index = '${i}'>&#xe5d4;</span>
+                  <span class="material-icons delete" data-index = '${i}'>&#xe872</span>
+                </div>
             </div>
     `;
     displayTasks.appendChild(displayTask);
 
     const checkbox = document.querySelector(`input[data-index="${i}"]`);
+    // eslint-disable-next-line no-loop-func
     checkbox.addEventListener('change', (event) => {
       const { index } = event.target.dataset;
       todoList[index].completed = event.target.checked;
       saveData();
     });
+
+    const editBtn = document.querySelector(`.edit[data-index="${i}"]`);
+    const deleteBtn = document.querySelector(`.delete[data-index="${i}"]`);
+    const descriptionElem = document.querySelector(`p[data-index="${i}"]`);
+    // eslint-disable-next-line no-loop-func
+    editBtn.addEventListener('click', (event) => {
+      const { index } = event.target.dataset;
+      const descriptionElem = document.querySelector(`p[data-index="${index}"]`);
+      descriptionElem.contentEditable = true;
+      descriptionElem.classList.add('edit-mode');
+      displayTask.style.backgroundColor = '#fff9c1';
+      descriptionElem.focus();
+      deleteBtn.style.display = 'block';
+      editBtn.style.display = 'none';
+      saveData();
+    });
+
+    // eslint-disable-next-line no-loop-func
+    descriptionElem.addEventListener('blur', (event) => {
+      const { index } = event.target.dataset;
+      todoList[index].description = event.target.textContent;
+      const descriptionElem = document.querySelector(`p[data-index="${index}"]`);
+      descriptionElem.contentEditable = false;
+      descriptionElem.classList.remove('edit-mode');
+      displayTask.style.backgroundColor = 'transparent';
+      editBtn.style.display = 'block';
+      saveData();
+    });
+
+    // eslint-disable-next-line no-loop-func
+    deleteBtn.addEventListener('click', (event) => {
+      const { index } = event.target.dataset;
+      todoList.splice(index, 1);
+      saveData();
+      renderList();
+    });
   }
 }
 
 function addTask(event) {
-    event.preventDefault();
-    const taskData = {
-      description: '',
-      completed: false,
-      index: todoList.length,
-    }
-    const getInput = addToList.value;
-    taskData.description = getInput;
-    todoList.push(taskData);
-    saveData();
-    renderList();
+  event.preventDefault();
+  const taskData = {
+    description: '',
+    completed: false,
+    index: todoList.length,
+  };
+  const getInput = addToList.value;
+  taskData.description = getInput;
+  todoList.push(taskData);
+  saveData();
+  renderList();
 
-    addToList.value = ''; //reset input field
-    location.reload(); //refresh page
+  addToList.value = ''; // reset input field
+  // eslint-disable-next-line no-restricted-globals
+  location.reload(); // refresh page
 }
 
 loadData();
 addBtn.addEventListener('click', addTask);
-refreshBtn.addEventListener('click', loadData);
+refreshBtn.addEventListener('click', () => {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+  loadData();
+});
 
 // eslint-disable-next-line import/prefer-default-export
 export { renderList };
