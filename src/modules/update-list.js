5797a5c8e7c0877c843067422302e7dd6a556cbb
Dave <davidolanrewaju538@gmail.com>
2023-04-08 11:12:46 +0100

    Update src files

---
+++ src/modules/update-list.js
@@ -0,0 +1,25 @@
+/* eslint-disable-next-line import/no-cycle */
+import { todoList, saveData } from './add-task.js';
+import { renderList } from './todolist.js';
+
+function checkTask(event) {
+  const { index } = event.target.dataset;
+  todoList[index].completed = event.target.checked;
+  saveData();
+}
+
+function clearCompleted() {
+  const incompleteTasks = todoList.filter((task) => !task.completed);
+  todoList.splice(0, todoList.length, ...incompleteTasks);
+
+  // update the index property of the remaining tasks
+  todoList.forEach((task, i) => {
+    task.index = i + 1;
+  });
+
+  saveData();
+  renderList();
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { checkTask, clearCompleted };
