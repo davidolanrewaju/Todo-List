0e8ba0c2a33059591eaf9a9445dde2f13d1098df
Dave <davidolanrewaju538@gmail.com>
2023-04-20 16:11:04 +0100

    Update file

--- src/tests/delete-task.test.js
+++ src/tests/delete-task.test.js
@@ -9,30 +9,38 @@ const todoList = [
 
 describe('deleteTask function', () => {
   test('should remove the correct task from the todoList array', () => {
-    // Mock the event object
-    const event = { target: { dataset: { index: 1 } } };
+    document.addEventListener('DOMContentLoaded', () => {
+      const displayTasks = document.querySelector('.display-tasks');
+      displayTasks.innerHTML = '';
+      // Mock the event object
+      const event = { target: { dataset: { index: 1 } } };
 
-    // Call the deleteTask function
-    deleteTask(event);
+      // Call the deleteTask function
+      deleteTask(event);
 
-    // Check if the correct task has been removed from the todoList array
-    expect(todoList).toEqual([
-      { title: 'Task 1', description: 'Description 1', index: 1 },
-      { title: 'Task 3', description: 'Description 3', index: 2 },
-    ]);
+      // Check if the correct task has been removed from the todoList array
+      expect(todoList).toEqual([
+        { title: 'Task 1', description: 'Description 1', index: 1 },
+        { title: 'Task 3', description: 'Description 3', index: 2 },
+      ]);
+    });
   });
 
-  //   test('should update the index property of the remaining tasks', () => {
-  //     // Mock the event object
-  //     const event = { target: { dataset: { index: 0 } } };
+  test('should update the index property of the remaining tasks', () => {
+    document.addEventListener('DOMContentLoaded', () => {
+      const displayTasks = document.querySelector('.display-tasks');
+      displayTasks.innerHTML = '';
+      // Mock the event object
+      const event = { target: { dataset: { index: 0 } } };
 
-  //     // Call the deleteTask function
-  //     deleteTask(event);
+      // Call the deleteTask function
+      deleteTask(event);
 
-//     // Check if the index property of the remaining tasks has been updated
-//     expect(todoList).toEqual([
-//       { title: 'Task 1', description: 'Description 1', index: 1 },
-//       { title: 'Task 3', description: 'Description 3', index: 2 },
-//     ]);
-//   });
+      // Check if the index property of the remaining tasks has been updated
+      expect(todoList).toEqual([
+        { title: 'Task 1', description: 'Description 1', index: 1 },
+        { title: 'Task 3', description: 'Description 3', index: 2 },
+      ]);
+    });
+  });
 });
